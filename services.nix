edcf4c94b1a097431f5ceed37c8860e54b932fda
Henri Menke <henrimenke@gmail.com>
2020-11-20 12:44:24 +0100

    Remove shadowsocksWithPlugins

--- services.nix
+++ services.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./declarative-channels/channels.nix
-    ./shadowsocks/shadowsocksWithPlugins.nix
     ./ssh-login-notify/ssh-login-notify.nix
     ./systemd-boot/systemd-boot.nix
     ./systemd-email-notify/systemd-email-notify.nix
