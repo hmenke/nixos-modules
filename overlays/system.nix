28582da283ce5ecb3bfd1c68a274e713482ce819
Henri Menke <henri@henrimenke.de>
2021-03-29 10:53:56 +0200

    Cache some derivations

--- overlays/system.nix
+++ overlays/system.nix
@@ -1,6 +1,6 @@
 final: prev: {
   adblock = final.callPackage ../pkgs/adblock { };
   drone-runner-docker = final.callPackage ../pkgs/drone-runner-docker { };
-  libfprint-tod = final.callPackage ../../pkgs/libfprint-tod/default.nix { };
-  libfprint-tod-goodix = final.callPackage ../../pkgs/libfprint-tod-goodix/default.nix { };
+  libfprint-tod = final.callPackage ../pkgs/libfprint-tod { };
+  libfprint-tod-goodix = final.callPackage ../pkgs/libfprint-tod-goodix { };
 }
