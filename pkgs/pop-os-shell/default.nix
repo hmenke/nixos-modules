67f7bce0ba7ed24a57460f05e1ca2c2e6644a854
Henri Menke <henri@henrimenke.de>
2021-07-23 14:27:08 +0200

    pop-os-shell: 1.3.0+git20210621.976106e -> 1.3.0+git20210719.9616931

--- pkgs/pop-os-shell/default.nix
+++ pkgs/pop-os-shell/default.nix
@@ -7,17 +7,17 @@
 , gjs
 }:
 let
-  rev = "976106e564acafcb3fad2a12a7d16f2c01e1f1ce";
+  rev = "9616931a2f8da33ff60d6332cbb1e4036c9ae2b3";
 in
 stdenv.mkDerivation rec {
   pname = "pop-os-shell";
-  version = "1.3.0+git20210621.${builtins.substring 0 7 rev}";
+  version = "1.3.0+git20210719.${builtins.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
     inherit rev;
-    sha256 = "sha256-pCu3O5TKwywGZFoQk4GQqEGxETqFJgf7ZgBPVQD6oCs=";
+    sha256 = "sha256-lfXsBfye5PeVIGmeo0M4Tg/KZcANImZAHQKFQx5xrrU=";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript gjs ];
