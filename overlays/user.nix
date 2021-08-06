b58300f2a8961b87860c901666e6fc63ceffbbcd
Henri Menke <henri@henrimenke.de>
2021-08-06 18:09:18 +0200

    Move softmaker-office overlay

--- overlays/user.nix
+++ overlays/user.nix
@@ -4,7 +4,7 @@ final: prev: {
   nix-direnv = prev.nix-direnv.override { enableFlakes = true; };
   nixos-shell = prev.nixos-shell.override { nix = final.nixFlakes; };
   pop-os-shell = final.callPackage ../pkgs/pop-os-shell { };
-  softmaker-office = prev.softmaker-office.override {
+  softmaker-office-2018-976 = final.softmaker-office.override {
     officeVersion = {
       version = "976";
       edition = "2018";
