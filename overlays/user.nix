f40c578c61cdef9ebae9d93156972b25a9caf4b0
Henri Menke <henri@henrimenke.de>
2021-12-03 16:24:26 +0100

    overlays/user: remove obsolete overrides and add some Flakes

--- overlays/user.nix
+++ overlays/user.nix
@@ -1,12 +1,11 @@
 final: prev: {
-  gajim = prev.gajim.overrideAttrs ({ buildInputs, ... }: {
-    buildInputs = buildInputs ++ [ final.libappindicator-gtk3 ];
-  });
   masterpdfeditor-free = final.callPackage ../pkgs/masterpdfeditor-free { };
   mathematica-env = final.callPackage ../pkgs/mathematica-env { };
   nix-direnv = prev.nix-direnv.override { enableFlakes = true; };
   nix-prefetch = prev.nix-prefetch.override { nix = final.nixFlakes; };
+  nix-update = prev.nix-update.override { nix = final.nixFlakes; };
   nixos-shell = prev.nixos-shell.override { nix = final.nixFlakes; };
+  nixpkgs-review = prev.nixpkgs-review.override { nix = final.nixFlakes; };
   pop-os-shell = final.callPackage ../pkgs/pop-os-shell { };
   softmaker-office-2018-976 = final.softmaker-office.override {
     officeVersion = {
@@ -15,6 +14,5 @@ final: prev: {
       hash = "sha256-A45q/irWxKTLszyd7Rv56WeqkwHtWg4zY9YVxqA/KmQ=";
     };
   };
-  splatmoji = final.callPackage ../pkgs/splatmoji { };
   texlive-env = final.callPackage ../pkgs/texlive-env { };
 }
