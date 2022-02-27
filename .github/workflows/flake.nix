759163cfd4e750615e0aca3d89acf85e699b8043
Henri Menke <henri@henrimenke.de>
2022-02-27 14:18:00 +0100

    Cache using flakes

---
+++ .github/workflows/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Binary cache";
+
+  inputs = {
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-branchless = {
+      url = "github:arxanas/git-branchless";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, emacs-overlay, git-branchless, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          emacs-overlay.overlay
+          git-branchless.overlay
+        ];
+      };
+    in {
+      checks.${system} = { inherit (pkgs) emacsPgtkGcc git-branchless; };
+  };
+}
