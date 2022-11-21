c4a3638961f159fbacd11b04d3444bba375cf3d0
Henri Menke <henri@henrimenke.de>
2022-11-21 21:20:31 +0100

    modules/deployment: remove in favor of sops-nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
 
     nixosModules = {
       adblock = import ./modules/adblock/module.nix;
-      deployment = import ./modules/deployment/module.nix;
       ssh-login-notify = import ./modules/ssh-login-notify/module.nix;
       systemd-boot = import ./modules/systemd-boot/systemd-boot.nix;
       systemd-email-notify = import ./modules/systemd-email-notify/module.nix;
