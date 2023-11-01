bdb89237b147d1b313f7e0015a33f1732414fe2e
Henri Menke <henri@henrimenke.de>
2023-11-01 16:01:51 +0100

    adblock: 3.13.26 -> 3.14.21
    Diff: https://github.com/StevenBlack/hosts/compare/3.13.26...3.14.21

--- pkgs/adblock/default.nix
+++ pkgs/adblock/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "adblock";
-  version = "3.13.26";
+  version = "3.14.21";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    sha256 = "sha256-dL3Twot4t5SnvDSXQmQC8qcs0mNIgsQNsHQw7sfsoOc=";
+    sha256 = "sha256-h0r9xNhMYHMzVDJSXDBO4obFSkUHXMQItxP0m3q1adc=";
   };
 
   installPhase = let
