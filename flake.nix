e775c35e02f2abd2655f86f5d1185f0411df3c37
Henri Menke <henri@henrimenke.de>
2024-12-24 15:19:33 +0100

    Clean up old attributes

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
   outputs = { self, ... }: {
     overlays = {
       system = import ./overlays/system.nix;
-      user = import ./overlays/user.nix;
     };
 
     nixosModules = {
       adblock = import ./modules/adblock/module.nix;
       ssh-login-notify = import ./modules/ssh-login-notify/module.nix;
-      systemd-boot = import ./modules/systemd-boot/systemd-boot.nix;
       systemd-email-notify = import ./modules/systemd-email-notify/module.nix;
       systemd-notify-send = import ./modules/systemd-notify-send/module.nix;
       zfs-auto-snapshot = import ./modules/zfs-auto-snapshot/module.nix;
